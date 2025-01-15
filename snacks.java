32fa111911c258872c690095045cff6f900e2c80
aubreycantrell <aubrey.cantrell@wellesley.edu>
2025-01-15 09:32:07 -0500

    more info about snacks

--- snacks.java
+++ snacks.java
@@ -1,7 +1,9 @@
 
 public class Snack {
     public static void main (String[] args){
-        System.out.println(x: "Snacks!")
+        System.out.println(x: "Snacks!");
+        System.out.println(x: "Hello! I like to eat lots of kind bars and gold fish and la croix");
+
     }
 
 }
